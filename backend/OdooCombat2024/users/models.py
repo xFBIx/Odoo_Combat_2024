ca2184f019c55fc41512f9924a270b3bedaaa1bc
Rudradatt Dave <rudradattdave@gmail.com>
2024-06-29 10:14:29 +0530

    user model created

---
+++ backend/OdooCombat2024/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    auth_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name or self.auth_user.username
